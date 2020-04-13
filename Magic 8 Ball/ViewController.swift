e471826b11d79bbe413116c8178bbf590d5a5739
leonjmac <leonjmac@protonmail.ch>
2020-04-13 23:00:56 +0100

    Update with animation and custom background.

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -11,11 +11,21 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var magicBallView: UIImageView!
+    @IBOutlet weak var askButton: UIButton!
+    
     let ballArray = [#imageLiteral(resourceName: "ball1"),#imageLiteral(resourceName: "ball2"),#imageLiteral(resourceName: "ball3"),#imageLiteral(resourceName: "ball4"),#imageLiteral(resourceName: "ball5")]
     
     @IBAction func shake(_ sender: Any) {
         self.magicBallView.image = ballArray.randomElement()
+        self.magicBallView.animateShake()
     }
+    
+    override func viewDidLoad() {
+        
+        
+        super.viewDidLoad()
+    }
+    
     
     override func becomeFirstResponder() -> Bool {
         return true
@@ -25,5 +35,14 @@ class ViewController: UIViewController {
         if motion == .motionShake {
             shake(motion)
         }
+    }
+}
+
+extension UIView {
+    func animateShake() {
+        self.transform = CGAffineTransform(translationX: 25, y: 0)
+        UIView.animate(withDuration: 0.4, delay: 0, usingSpringWithDamping: 0.2, initialSpringVelocity: 1, options: .curveEaseInOut, animations: {
+            self.transform = CGAffineTransform.identity
+        }, completion: nil)
     }
 }
