445c42332e67536ee4e0dd82c02ed077e49358e1
leonjmac <leonjmac@protonmail.ch>
2020-04-18 23:33:37 +0100

    Update with curved diamond factory function.

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -15,10 +15,33 @@ class ViewController: UIViewController {
     @IBOutlet weak var askButton: UIButton!
     
     let fortunesArray = ["Don\'t count on it", "Seems unlikely", "Perhaps", "Ask me later", "Yes", "Nope", "¯\\_(ツ)_/¯" ]
+    var starsArray: [CAShapeLayer] = []
+    
+    override func viewDidLoad() {
+        // Generate 10 stars
+        for i in 0..<10 {
+            // Randomise size
+            self.starsArray.append(drawDiamond(CGSize(width: 75, height: 100)))
+        }
+    }
     
     @IBAction func shake(_ sender: Any) {
-        self.magicBallView.animateShake()
-        self.magicBallLabel.text = fortunesArray.randomElement()?.uppercased()
+        self.askButton.isEnabled.toggle()
+        self.swapMagicLabelText((fortunesArray.randomElement()?.uppercased())!)
+        self.magicBallView.animateShake(withCompletion: { finished in
+            self.askButton.isEnabled = finished
+        })
+    }
+    
+    func swapMagicLabelText(_ value: String) {
+        UIView.animate(withDuration: 0.5, delay: 0, options: [], animations: {
+            self.magicBallLabel.alpha = 0.0
+        }, completion: { finished in
+            self.magicBallLabel.text = value
+            UIView.animate(withDuration: 0.5, animations: {
+                self.magicBallLabel.alpha = 1.0
+            })
+        })
     }
     
     override func becomeFirstResponder() -> Bool {
@@ -30,35 +53,27 @@ class ViewController: UIViewController {
             shake(motion)
         }
     }
+    
+    func drawDiamond(_ size: CGSize) -> CAShapeLayer {
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x:size.width / 2.0 , y:0.0))
+        path.addQuadCurve(to: CGPoint(x: size.width, y: size.height / 2.0), controlPoint: CGPoint(x: 0.625 * size.width , y: 0.375 * size.height))
+        path.addQuadCurve(to: CGPoint(x: size.width / 2.0 , y: size.height), controlPoint: CGPoint(x: 0.625 * size.width , y: 0.625 * size.height))
+        path.addQuadCurve(to: CGPoint(x: 0.0 , y: size.height / 2.0), controlPoint: CGPoint(x: 0.375 * size.width, y: 0.625 * size.height))
+        path.addQuadCurve(to: CGPoint(x: size.width / 2.0 , y: 0.0), controlPoint: CGPoint(x: 0.375 * size.width, y: 0.375 * size.height))
+
+        let shape = CAShapeLayer()
+        shape.path = path.cgPath
+        shape.fillColor = UIColor.yellow.cgColor
+        return shape
+    }
 }
 
 extension UIView {
-    func animateShake() {
-        // Down
-        self.transform = CGAffineTransform(translationX: 0, y: 25)
-        UIView.animate(withDuration: 0.4, delay: 0, usingSpringWithDamping: 0.4, initialSpringVelocity: 1, options: .curveEaseInOut, animations: {
+    func animateShake(withCompletion completion:((Bool) -> Void)?) {
+        self.transform = CGAffineTransform(translationX: 0, y: 35)
+        UIView.animate(withDuration: 1.25, delay: 0, usingSpringWithDamping: 0.4, initialSpringVelocity: 0.5, options: .curveEaseIn, animations: {
             self.transform = CGAffineTransform.identity
-        }, completion: nil)
-        
-        
-        // Delay by 0.5 sec then
-        
-                
-        // Down + fade text to 0.5
-        
-        // Up + fade text to 0.0
-        
-        // Down + bubble up
-        
-        // Up + bubble up
-        
-        // Down + bubble up
-        
-        // Up + bubble up + fade new text to 0.5
-        
-        // Down + bubble up + new text to 1
-        
-        // Rest + bubble up
-        
+        }, completion: completion)
     }
 }
